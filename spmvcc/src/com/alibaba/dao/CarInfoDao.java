dbb26ead7b19b7e5351d8626309e86b604c511e7
litong <litong@xinxindai.com>
2017-02-23 15:03:38 +0800

    测试提交git

--- spmvcc/src/com/alibaba/dao/CarInfoDao.java
+++ spmvcc/src/com/alibaba/dao/CarInfoDao.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Param;
 import com.alibaba.entity.CarInfoTable;
 
 public interface CarInfoDao {
-	//查询所有车辆信息
+	//查询所有车辆信息  测试git我修改一下然后提交看看什么效果
 	List<CarInfoTable> queryCarInfo();
 	//根据工单号查询车辆信息
 	List<CarInfoTable> queryCarInfoByOrderCode(@Param("orderCode")String orderCode,@Param("carName")String carName);
